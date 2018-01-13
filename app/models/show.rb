19aee28949caad9ae513ff04d1b81b96830fa945
ronald vilorio <soccer23415@gmail.com>
2018-01-13 04:25:39 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,13 +2,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
-  # def build_network(attributes)
-  #   attributes
-  # end
-  def network
-
-
+  def build_network(attributes)
+    attributes
   end
-
 
 end
